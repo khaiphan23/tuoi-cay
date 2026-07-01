9fa2243365542113153a416446a2918cccc55b5d
khaiphan23 <khaidatrang@gmail.com>
2026-07-01 21:02:28 +0700

    Remove duplicated nested repository

--- smart-irrigation/flutter_app/lib/core/constants.dart
+++
@@ -1,4 +0,0 @@
-
-const String supabaseUrl = 'https://dqpysplrolkkljtbebta.supabase.co';
-const String supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxcHlzcGxyb2xra2xqdGJlYnRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2Nzg1NzMsImV4cCI6MjA5ODI1NDU3M30.2zv7UcMVhMoEMF55ZLx5X4wsEqTQ-SCO8aXtxOf9rHQ';
-const String deviceId   = '9c0eb412-1ba5-4d3c-a1f3-cce6cb58082b';
